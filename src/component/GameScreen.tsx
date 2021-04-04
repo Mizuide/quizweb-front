97b220e7e2f4d169b7c004fd25c2f69359af9fa3
root <waterexot1120@gmai.com>
2021-04-04 11:03:55 +0000

    eject + test

--- src/component/GameScreen.tsx
+++ src/component/GameScreen.tsx
@@ -1,14 +1,20 @@
+import axios from 'axios';
 
+type prop ={
+    id:string
+}
 
-function GameScreen(){
+function GameScreen(props:prop){
+   let num:string = fetchQuestion(props.id);
     return(
-        <div className="GameScreen"></div>
+        
+        <div className="GameScreen">{num}</div>
     )
 
 }
 
-// function fetchQuestion(id:BigInteger):String{
-//     return
-// }
+function fetchQuestion(id:string):string{
+    return id;
+}
 
 export default GameScreen;
--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GameScreen />
+    <GameScreen id='hogehoge'/>
   </React.StrictMode>,
   document.getElementById('root')
 );
