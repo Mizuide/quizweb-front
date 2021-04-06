db6f05fad005e6c5e7878c49d303d3ee18654300
root <root@DESKTOP-0RBL630.localdomain>
2021-04-06 09:16:45 +0900

    a

--- src/component/GameScreen.tsx
+++ src/component/GameScreen.tsx
@@ -1,31 +1,31 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 
-type prop ={
-    id:string
-}
+// type prop ={
+//     id:string
+// }
 
-type question ={
-    id:String,
-    content:String
-}
+// type question ={
+//     id:String,
+//     content:String
+// }
 
-function GameScreen(props:prop){
-   let num:question = fetchQuestion(props.id);
-    return(
+// function GameScreen(props:prop){
+//    let num:question = fetchQuestion(props.id);
+//     return(
         
-        <div className="GameScreen">{num.content}</div>
-    )
+//         <div className="GameScreen">{num.content}</div>
+//     )
 
-}
+// }
 
 
 
- function fetchQuestion(id:string):question{
-    let question:question;
-    axios.get("").then(res => {return JSON.parse(res.data)})
-    // return question;
+//  function fetchQuestion(id:string):question{
+//     let question:question;
+//     axios.get("").then(res => {return JSON.parse(res.data)})
+//     // return question;
 
-}
+// }
 
-export default GameScreen;
+// export default GameScreen;
