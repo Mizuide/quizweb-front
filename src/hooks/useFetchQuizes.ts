bf531c369492df394814d77a3b0a03bd56e88d75
water <waterexit1120@gmail.com>
2021-08-19 11:56:02 +0900

    fetch fix

--- src/hooks/useFetchQuizes.ts
+++ src/hooks/useFetchQuizes.ts
@@ -2,25 +2,23 @@ import axios, { AxiosResponse } from 'axios';
 import { useState } from "react";
 import quiz from "../type/quiz";
 import fetchQuizParam from "../type/fetchQuizParam";
+import api from "../property/api.json"
 
-const QUIZ_URL: string = "/quizWeb/quiz/get";
+type returnQuizesInfo = {
+    count:number,
+    quizes:quiz[]
+}
 
-function fetchQuizes(param: fetchQuizParam): Promise<AxiosResponse<quiz[]>> {
-    return axios.post<quiz[]>(QUIZ_URL, { ...param });
+function fetchQuizes(param: fetchQuizParam): Promise<AxiosResponse<returnQuizesInfo>> {
+    return axios.post<returnQuizesInfo>(api.quiz.url, { ...param });
 
 }
 
-const useFetchQuizes: () => [quiz[], (prop: fetchQuizParam) => void] = () => {
-    const [quizes, setQuizes] = useState<quiz[]>([]);
+const useFetchQuizes: () => [returnQuizesInfo, (prop: fetchQuizParam) => void] = () => {
+    const [quizes, setQuizes] = useState<returnQuizesInfo>({quizes:[],count:0});
+    
     const setFetchQuiz = function (prop: fetchQuizParam) {
-        fetchQuizes(prop).
-            then(res => {
-                if (prop.lastQuiz === null) {
-                    return res.data;
-                } else {
-                    return quizes.concat(res.data);
-                }
-            }).then(newQuizes => setQuizes(newQuizes));
+        fetchQuizes(prop).then(res => setQuizes(res.data));
     }
 
     return [quizes, setFetchQuiz];
