81e13e0c27407f9f27e71eb3792d52e433ced4cb
water <waterexit1120@gmail.com>
2021-08-19 09:40:12 +0900

    designed search box

--- src/component/WhereTitle.tsx
+++ src/component/WhereTitle.tsx
@@ -1,15 +1,17 @@
 import { ReactElement } from "react";
+import { Input } from "semantic-ui-react";
 
 interface WhereTitle extends ReactElement { }
 
 type prop = {
     setWhereTitle: (title: string) => void
+    onButtonClick:() => void
 }
 
 const WhereTitle: React.FC<prop> = (prop) => {
     return (
         <div className='whereTitle'>
-            <input type='search' className='input' onChange={(e) => prop.setWhereTitle(e.target.value)} />
+            <Input onChange={(e) => prop.setWhereTitle(e.target.value)} action={{"content":'検索',"icon":'search','onClick':prop.onButtonClick }} />
         </div>
     )
 }
