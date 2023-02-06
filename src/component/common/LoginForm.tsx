5a42968c9a239f4052d523b63a439ce3e60923be
water <waterexit1120@gmail.com>
2023-02-06 15:29:35 +0000

    fix

--- src/component/common/LoginForm.tsx
+++ src/component/common/LoginForm.tsx
@@ -2,26 +2,27 @@ import axios from 'axios';
 import React, { useContext, useEffect, useState } from "react";
 import { loginUserContext } from "../../App";
 import loginUser from "../../type/loginUser";
+import api from "../../property/api.json";
 
-const LOGIN_URL = "/quizWeb/login";
+
 
 type prop = {
-   setLoginUser: React.Dispatch<React.SetStateAction<loginUser|undefined>>
+  setLoginUser: React.Dispatch<React.SetStateAction<loginUser | undefined>>
 }
 
 const LoginForm: React.FC<prop> = (prop: prop) => {
-    const loginUser = useContext(loginUserContext)
-    const [message,setMessage] = useState<string>('ログイン処理中…');
-    useEffect(() => {
-        axios.get(LOGIN_URL).then(res => prop.setLoginUser(res.data)).catch(e => prop.setLoginUser(undefined));
-    },[])
+  const loginUser = useContext(loginUserContext)
+  const [message, setMessage] = useState<string>('ログイン処理中…');
+  useEffect(() => {
+    axios.get(api.login.url).then(res => prop.setLoginUser(res.data)).catch(e => prop.setLoginUser(undefined));
+  }, [])
 
-    useEffect(() => {
-        if(loginUser !== undefined)
-            setMessage(`ログインしました　ようこそ${loginUser.name}さん`)
-    },[loginUser])
+  useEffect(() => {
+    if (loginUser !== undefined)
+      setMessage(`ログインしました　ようこそ${loginUser.name}さん`)
+  }, [loginUser])
 
-return(
+  return (
     <div>
       {message}
     </div>
